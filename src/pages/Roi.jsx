cd5e352bb0d58b2f6d99c8ecc373939a0a6af0b0
Nitish Kumar <nitish@infiniticube.in>
2023-07-26 11:43:23 +0530

    Initial commit

--- src/pages/Roi.jsx
+++ src/pages/Roi.jsx
@@ -103,7 +103,8 @@ const Roi = ({ setShowCalculator }) => {
           </button>
         </div>
         <div className="flex justify-end">
-          <div className="text-bold text-slate-500 mb-4">
+          <div className=" flex text-bold text-slate-500 mb-4">
+            <img src="https://ih1.redbubble.net/image.2331639090.7433/st,small,507x507-pad,600x600,f8f8f8.jpg" width={20} height={20} alt="" />
             <span className="mx-3">Cake</span>
             <input
               className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-yellow-500 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-neutral-300 checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5  checked:after:border-none checked:after:bg-yellow-100 hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1]  focus:after:content-[''] checked:focus:border-yellow-600 checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100  dark:bg-yellow-600 dark:after:bg-neutral-400 dark:checked:bg-yellow-100 dark:checked:after:bg-yellow-500 dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)]"
