619036b6c47f48dadc24e94f56b5561b8c156741
Nitish Kumar <nitish@infiniticube.in>
2023-07-26 09:00:05 +0530

    Initial commit

--- src/pages/Roi.jsx
+++ src/pages/Roi.jsx
@@ -1,36 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { PiPencilSimpleLight } from "react-icons/pi";
+import { BsCheck2 } from "react-icons/bs";
 import { LiaAngleDownSolid, LiaAngleRightSolid } from "react-icons/lia";
 
 const Roi = ({ setShowCalculator }) => {
   const [showDetails, setshowDetails] = useState(false);
   const [input, setInput] = useState({ cake: 0, usd: 0 });
-  const roiforTimeFrame = {day1: "63.34", day7: "33.12", day30: "23.56", year1: "17.83", year5: "14.67"}
-  const [chooseRoi, setchooseRoi] = useState(roiforTimeFrame.day1)
+  const roiforTimeFrame = {
+    day1: "63.34",
+    day7: "33.12",
+    day30: "23.56",
+    year1: "17.83",
+    year5: "14.67",
+  };
+  const [chooseRoi, setchooseRoi] = useState(roiforTimeFrame.day1);
+  const [roiArg, setRoiArg] = useState({investment: 0, apy: 63.34, timeframe: 1})
   const onChange = (e) => {
     setInput({ ...input, [e.target.name]: `${e.target.value}` });
   };
   const activebtn = (clases, id) => {
-    if(id==="active4"){
-        setchooseRoi(roiforTimeFrame.day1)
+    if (id === "active4") {
+      setchooseRoi(roiforTimeFrame.day1);
+      setRoiArg({...roiArg, apy: 63.34, timeframe: 1})
+    } else if (id === "active5") {
+      setchooseRoi(roiforTimeFrame.day7);
+      setRoiArg({...roiArg, apy: 33.12, timeframe: 7})
 
-    }else if(id==="active5"){
-        setchooseRoi(roiforTimeFrame.day7)
-    }else if(id==="active6"){
-        setchooseRoi(roiforTimeFrame.day30)
-    }else if(id==="active7"){
-        setchooseRoi(roiforTimeFrame.year1)
-    }else if(id==="active8"){
-        setchooseRoi(roiforTimeFrame.year5)
+    } else if (id === "active6") {
+      setchooseRoi(roiforTimeFrame.day30);
+      setRoiArg({...roiArg, apy: 23.56, timeframe: 30})
+
+    } else if (id === "active7") {
+      setchooseRoi(roiforTimeFrame.year1);
+      setRoiArg({...roiArg, apy: 17.83, timeframe: "1year"})
+
+    } else if (id === "active8") {
+      setchooseRoi(roiforTimeFrame.year5);
+      setRoiArg({...roiArg, apy: 14.67, timeframe: "5year"})
+
     }
     document.querySelectorAll(clases).forEach((e) => {
       e.classList.remove("active");
     });
     document.getElementById(id).classList.add("active");
-
   };
-  console.log(chooseRoi)
+
+  const [estimate, setEstimate] = useState({ cake: 0, usd: 0 });
+
+  useEffect(() => {
+    setEstimate({ ...estimate, usd: +input.cake * 2, cake: input.usd / 2 });
+    // setRoiArg({...roiArg, investment: +input.usd})
+    calculateProfit(+input.usd, roiArg.apy, roiArg.timeframe)
+  }, [input]);
+
+  function calculateProfit(investment, apy, timeframe) {
+    if (timeframe > 0 && timeframe < 366) {
+      let profitbytimeFram = 365 / timeframe;
+      // Calculate the profit.
+      let profit = ((investment * apy) / 100) / profitbytimeFram;
+
+      // Return the profit.
+      setInput({...input, cake: (profit/2).toFixed(4)})
+    } else {
+      // Calculate the profit.
+      let profit = ((investment * apy) / 100) * +(timeframe[0]);
+
+      setInput({...input, cake: (profit/2).toFixed(4)})
+    }
+  }
+
+  const [isPencilIcon, setIsPencilIcon] = useState(true)
+
+  const onFocus = () => setIsPencilIcon(false);
+  const onBlur = () => setIsPencilIcon(true);
+
   return ReactDOM.createPortal(
     <div className="modal">
       <div className="modal-body">
@@ -60,13 +104,16 @@ const Roi = ({ setShowCalculator }) => {
         </div>
         <div className="flex flex-col justify-center ">
           <div className=" flex w-full relative">
-            <span className="text-black text-xl font-extrabold absolute top-3 right-2">CAKE</span>
+            <span className="text-black text-xl font-extrabold absolute top-3 right-2">
+              CAKE
+            </span>
             <input
-              type="text"
+              type="number"
               name="cake"
               className="appearance-none block w-full bg-gray-100  border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-right text-black text-xl font-extrabold pr-16"
               value={input.cake}
               onChange={onChange}
+              disabled
             />
           </div>
           <div className="w-100 flex justify-between">
@@ -74,29 +121,35 @@ const Roi = ({ setShowCalculator }) => {
               <span
                 id="active1"
                 className="isrmvactive1 active cursor-pointer uppercase text-sm font-bold p-2 m-2 ml-0 bg-slate-100 rounded-md"
-                onClick={() => {activebtn(".isrmvactive1", "active1"); setInput({...input
-                    , usd: 0})}}
+                onClick={() => {
+                  activebtn(".isrmvactive1", "active1");
+                  setInput({ ...input, usd: 0 });
+                }}
               >
                 use balance
               </span>
               <span
                 id="active2"
                 className="isrmvactive1 cursor-pointer  p-2 m-2 bg-slate-100 rounded-md"
-                onClick={() => {activebtn(".isrmvactive1", "active2"); setInput({...input
-                    , usd: 1000})}}
+                onClick={() => {
+                  activebtn(".isrmvactive1", "active2");
+                  setInput({ ...input, usd: 1000 });
+                }}
               >
                 $1000
               </span>
               <span
                 id="active3"
                 className="isrmvactive1 cursor-pointer  p-2 m-2 bg-slate-100 rounded-md"
-                onClick={() => {activebtn(".isrmvactive1", "active3"); setInput({...input
-                , usd: 100})}}
+                onClick={() => {
+                  activebtn(".isrmvactive1", "active3");
+                  setInput({ ...input, usd: 100 });
+                }}
               >
                 $100
               </span>
             </div>
-            <span>~ $100</span>
+            <span>~ ${estimate.usd}</span>
           </div>
         </div>
         <div className="w-100 mt-4 flex flex-col justify-between">
@@ -208,26 +261,32 @@ const Roi = ({ setShowCalculator }) => {
           </div>
           <div className="w-full relative">
             <div className="absolute top-4 left-4">
-              <PiPencilSimpleLight className="text-2xl text-slate-400" />
+              {isPencilIcon?<PiPencilSimpleLight className="text-2xl text-slate-400" />:<BsCheck2 className="text-2xl text-slate-400"/>}
             </div>
-            <span className="text-black text-2xl font-extrabold absolute top-3 right-2">USD</span>
+            <span className="text-black text-2xl font-extrabold absolute top-3 right-2">
+              USD
+            </span>
             <input
-              type="text"
+              type="number"
               name="usd"
               className="pl-64 appearance-none block w-full bg-gray-100 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-right text-black text-2xl font-extrabold pr-16"
               value={input.usd}
               onChange={onChange}
+              onBlur={onBlur}
+              onFocus={onFocus}
             />
           </div>
           <div className="w-100 flex justify-end">
-            <span className="text-sm text-slate-400 font-bold">~ $100</span>
+            <span className="text-sm text-slate-400 font-bold">
+              ~ {estimate.cake} CAKE
+            </span>
           </div>
         </div>
         <div className="flex mt-4 w-full justify-between">
           <button className="pt-3 pb-3 pr-20 pl-20 rounded-lg bg-gray-800 text-white">
             Apply
           </button>
-          <button className="pt-3 pb-3 pr-20 pl-20 rounded-lg bg-slate-100 text-gray-800 border-gray-800 border-2">
+          <button className="pt-3 pb-3 pr-20 pl-20 rounded-lg bg-slate-100 text-gray-800 border-gray-800 border-2" onClick={()=>setInput({ cake: 0, usd: 0 })}>
             Cancel
           </button>
         </div>
@@ -245,7 +304,9 @@ const Roi = ({ setShowCalculator }) => {
           <div className="flex flex-col w-full">
             <div className="flex w-full justify-between">
               <span className="text-black font-bold">APY</span>
-              <span className="text-yellow-400 font-bold text-2xl">{chooseRoi}%</span>
+              <span className="text-yellow-400 font-bold text-2xl">
+                {chooseRoi}%
+              </span>
             </div>
             <div>
               <ul>
